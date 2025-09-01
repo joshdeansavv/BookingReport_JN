61d4867ac8bad61b1759b5f3cab5282685188b05
Joshua West <joshua@Joshuas-MacBook-Pro.local>
2025-09-01 15:43:20 -0600

    Update setup.sh to install packages for both Python 3.12 and 3.13

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir -p new archive
 pip3 install --user --break-system-packages PyMuPDF pdfplumber pillow requests
+/opt/homebrew/bin/python3.12 -m pip install --user --break-system-packages PyMuPDF pdfplumber pillow requests
 chmod +x run.py
